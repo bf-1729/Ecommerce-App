04a410df8f6f78e2d2b69b6d64de2ab90f45ed58
lakshmi narasimha <mlakshminarasimh95a@gmail.com>
2025-02-04 11:28:28 +0530

    Initial commit or updates

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -123,10 +123,6 @@ const PlaceOrder = () => {
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-500' : ''}`}></p>
               <img src={assets.stripe_logo} className='h-5 mx-4' alt=''></img>
             </div>
-            <div onClick={()=>setMethod('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-green-500' : ''}`}></p>
-              <img src={assets.razorpay_logo} className='h-5 mx-4' alt=''></img>
-            </div>
             <div onClick={()=>setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
               <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-500' : ''}`}></p>
               <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
