29a2a76c7eda9b75db888b1f1fb4f68a8485073a
lakshmi narasimha <mlakshminarasimh95a@gmail.com>
2025-02-05 20:14:35 +0530

    intial commit or updates

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ connectCloudinary()
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'https://buyease-frontend-tau.vercel.app',  // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 //api endpoints
 app.use("/api/user",userRouter)
