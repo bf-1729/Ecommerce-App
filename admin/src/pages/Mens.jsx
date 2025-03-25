6441ee5fbc94f0a2021d52e7cf32dd6465612fa8
lakshmi narasimha <mlakshminarasimh95a@gmail.com>
2025-03-25 11:55:34 +0530

    intial commit

--- admin/src/pages/Mens.jsx
+++ admin/src/pages/Mens.jsx
@@ -60,8 +60,8 @@ const Mens = ({token}) => {
         </div>
         {
             MensList.map((item,index)=>(
-                <div className='grid grid-cols-[0.5fr_1fr] sm:grid-cols-[1fr_3.1fr_1.1fr_1fr] items-center gap-2 py-1 px-2 border border-gray-300 text-sm product-item ${highlightedId === item._id ? "highlight" : ""}' key={index}>
-                    <img className='w-14' src={item.image[0]} alt=''></img>
+                <div className='grid grid-cols-[0.4fr_1fr] sm:grid-cols-[1fr_3.1fr_1.1fr_1fr] items-center gap-2 py-1 px-2 border border-gray-300 text-sm product-item ${highlightedId === item._id ? "highlight" : ""}' key={index}>
+                    <img className='w-12' src={item.image[0]} alt=''></img>
                     <p className='break-all text-[13px] sm:text-md'>{item.name}</p>
                     <p>{currency}{item.price}</p>
                     <p className='flex text-right lg:ml-0 ml-40 lg:justify-center gap-2 md:text-center cursor-pointer text-lg w-full'>
--- admin/src/pages/Womens.jsx
+++ admin/src/pages/Womens.jsx
@@ -4,78 +4,79 @@ import { backendUrl, currency } from '../App'
 import { toast } from 'react-toastify'
 import { Link } from 'react-router-dom'
 import ScrolltoTop from '../components/ScrolltoTop'
-const Womens = ({ token }) => {
 
-    const [list, setList] = useState([])
+const Womens = ({token}) => {
 
-    const fetchList = async () => {
-        try {
-            const response = await axios.get(backendUrl + "/api/product/list")
-            if (response.data.success) {
-                setList(response.data.products)
+    const [list,setList] = useState([])
+
+    const fetchList = async()=>{
+        try{
+            const response = await axios.get(backendUrl+"/api/product/list")
+            if(response.data.success){
+            setList(response.data.products)
             }
-            else {
+            else{
                 toast.error(response.data.message)
             }
-        } catch (error) {
+        }catch(error){
             console.log(error)
             toast.error(error.message)
         }
 
     }
 
-    const removeProduct = async (id) => {
-        try {
-            const response = await axios.post(backendUrl + "/api/product/remove", { id }, { headers: { token } })
-            if (response.data.success) {
+    const removeProduct = async(id)=>{
+        try{
+            const response = await axios.post(backendUrl+"/api/product/remove",{id},{headers:{token}})
+            if(response.data.success){
                 toast.success(response.data.message)
                 await fetchList();
             }
-            else {
+            else{
                 toast.error(response.data.message)
             }
         }
-        catch (error) {
-
+        catch(error){
+            
         }
     }
-    const WomensList = list.filter(item => item.category.includes("Women"))
+    const WomensList = list.filter(item=>item.category.includes("Women"))
 
-    useEffect(() => {
+    useEffect(()=>{
         fetchList()
-    }, [])
-    return (
-        <div className='sm:w-full w-fit overflow-x-hidden'>
-            <p className='flex flex-row justify-between mb-2'>
-                <p>All Products</p>
-                <p>Items: {WomensList.length}</p>
-            </p>
-            <div className='flex flex-col gap-2'>
-                <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
-                    <b>Image</b>
-                    <b>Name</b>
-                    <b>Price</b>
-                    <b className='text-center'>Action</b>
-                </div>
-                {
-                    WomensList.map((item, index) => (
-                        <div className='grid grid-cols-[0.3fr_1fr] sm:grid-cols-[1fr_3.2fr_1.1fr_1fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
-                            <img className='w-12' src={item.image[0]} alt=''></img>
-                            <p>{item.name}</p>
-                            <p>{currency}{item.price}</p>
-                            <p className='flex text-right lg:ml-0 ml-48 lg:justify-center gap-2 md:text-center cursor-pointer text-lg w-full'>
-                                <img className='lg:w-5 w-4 lg:h-5 h-4' onClick={() => removeProduct(item._id)} src="https://img.icons8.com/material-rounded/24/trash.png" alt="trash" />
-                                <Link to={`/edititem/${item._id}`}>
-                                    <img className='lg:w-5 w-4 lg:h-5 h-4' src="https://img.icons8.com/ios-glyphs/24/edit--v1.png" alt="edit--v1" />
-                                </Link>
-                            </p>
-                        </div>
-                    ))
-                }
-            </div>
-            <ScrolltoTop/>
+    },[])
+  return (
+    <div className='sm:w-full w-full overflow-x-hidden'>
+    <div className='flex flex-row justify-between mb-2'>
+        <p>All Products</p>
+        <p>Items: {WomensList.length}</p>
+    </div>
+    <div className='flex flex-col gap-2'>
+        <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
+        <b>Image</b>
+        <b>Name</b>
+        <b>Price</b>
+        <b className='text-center'>Action</b>
         </div>
-    )
+        {
+            WomensList.map((item,index)=>(
+                <div className='grid grid-cols-[0.4fr_1fr] sm:grid-cols-[1fr_3.1fr_1.1fr_1fr] items-center gap-2 py-1 px-2 border border-gray-300 text-sm product-item ${highlightedId === item._id ? "highlight" : ""}' key={index}>
+                    <img className='w-12' src={item.image[0]} alt=''></img>
+                    <p className='break-all text-[13px] sm:text-md'>{item.name}</p>
+                    <p>{currency}{item.price}</p>
+                    <p className='flex text-right lg:ml-0 ml-40 lg:justify-center gap-2 md:text-center cursor-pointer text-lg w-full'>
+                        <img className='lg:w-5 w-4 lg:h-5 h-4' onClick={()=>removeProduct(item._id)} src="https://img.icons8.com/material-rounded/24/trash.png" alt="trash"/>
+                        <Link to={`/edititem/${item._id}`}>
+                        <img className='lg:w-5 w-4 lg:h-5 h-4' src="https://img.icons8.com/ios-glyphs/24/edit--v1.png" alt="edit--v1"/>
+                        </Link>
+                        </p>
+                    </div>
+            ))
+        }
+    </div>
+    <ScrolltoTop/>
+    </div>
+  )
 }
 
 export default Womens
