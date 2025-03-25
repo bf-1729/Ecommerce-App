55af980973b86bb640c20640bf54cde4e8da41d2
lakshmi narasimha <mlakshminarasimh95a@gmail.com>
2025-03-25 11:34:43 +0530

    intial commit

--- admin/src/pages/Mens.jsx
+++ admin/src/pages/Mens.jsx
@@ -46,7 +46,7 @@ const Mens = ({token}) => {
         fetchList()
     },[])
   return (
-    <div className='sm:w-full w-fit overflow-x-hidden'>
+    <div className='sm:w-full w-94 overflow-x-hidden'>
     <div className='flex flex-row justify-between mb-2'>
         <p>All Products</p>
         <p>Items: {MensList.length}</p>
